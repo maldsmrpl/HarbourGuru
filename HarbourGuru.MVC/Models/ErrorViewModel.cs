3c706d56c1ed79e22cd5089a8314947faca64c3c
malds <malkov.dmytro@gmail.com>
2024-05-23 19:02:25 +0200

    Add project files.

---
+++ HarbourGuru.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HarbourGuru.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
