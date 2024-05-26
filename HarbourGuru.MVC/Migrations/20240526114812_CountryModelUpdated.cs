f20c80ba4a74cfa154733132c805a021f0944490
malds <malkov.dmytro@gmail.com>
2024-05-26 14:00:59 +0200

    Country model updated

---
+++ HarbourGuru.MVC/Migrations/20240526114812_CountryModelUpdated.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HarbourGuru.MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountryModelUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryCode",
+                table: "Countries",
+                newName: "CountryCodeA2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCodeA3",
+                table: "Countries",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryFlagLarge",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryFlagSmall",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryFullName",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCodeA3",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "CountryFlagLarge",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "CountryFlagSmall",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "CountryFullName",
+                table: "Countries");
+
+            migrationBuilder.RenameColumn(
+                name: "CountryCodeA2",
+                table: "Countries",
+                newName: "CountryCode");
+        }
+    }
+}
--- HarbourGuru.MVC/Models/Country.cs
+++ HarbourGuru.MVC/Models/Country.cs
@@ -9,11 +9,24 @@ namespace HarbourGuru.MVC.Models
 
         [Required]
         [MaxLength(2)]
-        public string CountryCode { get; set; }
-        
+        public string CountryCodeA2 { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string CountryCodeA3 { get; set; }
+
+        [Required]
+        public string CountryFlagSmall { get; set; }
+
+        [Required]
+        public string CountryFlagLarge { get; set; }
+
         [Required]
         public string CountryName { get; set; }
-        
+
+        [Required]
+        public string CountryFullName { get; set; }
+
         public ICollection<Harbour> Harbours { get; set; } = new List<Harbour>();
     }
 }
