7195c2e7ca4fa02f39b448c78a5ea9ffb4234aed
malds <malkov.dmytro@gmail.com>
2024-05-26 16:04:56 +0200

    generic repository and unit of work added

---
+++ HarbourGuru.MVC/Repository/UnitOfWork.cs
@@ -0,0 +1,83 @@
+﻿using CloudinaryDotNet;
+using HarbourGuru.MVC.Data;
+using HarbourGuru.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HarbourGuru.MVC.Repository
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly ApplicationDbContext context;
+        private GenericRepository<Country> countryRepository;
+        private GenericRepository<Harbour> harbourRepository;
+        private GenericRepository<HarbourCard> harbourCardRepository;
+
+        public UnitOfWork(DbContextOptions<ApplicationDbContext> options)
+        {
+            context = new ApplicationDbContext(options);
+        }
+
+        public GenericRepository<Country> CountryRepository
+        {
+            get
+            {
+
+                if (this.countryRepository == null)
+                {
+                    this.countryRepository = new GenericRepository<Country>(context);
+                }
+                return countryRepository;
+            }
+        }
+
+        public GenericRepository<Harbour> HarbourRepository
+        {
+            get
+            {
+
+                if (this.harbourRepository == null)
+                {
+                    this.harbourRepository = new GenericRepository<Harbour>(context);
+                }
+                return harbourRepository;
+            }
+        }
+        public GenericRepository<HarbourCard>HarbourCardRepository
+        {
+            get
+            {
+
+                if (this.harbourCardRepository == null)
+                {
+                    this.harbourCardRepository = new GenericRepository<HarbourCard>(context);
+                }
+                return harbourCardRepository;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
